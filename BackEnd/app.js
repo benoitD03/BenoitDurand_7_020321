10ddb3da780cc11c999b043cf557b8d279886eff
benoitD03 <bens.92@hotmail.fr>
2021-03-14 19:57:47 +0100

    script createMessage ok ssauf valeur par défaut

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 const messageRoutes = require('./routes/message');
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use(cors());
 
   app.use('/api/users', userRoutes);
   app.use('/api/messages', messageRoutes);
--- frontend/src/components/CreateMessage.vue
+++ frontend/src/components/CreateMessage.vue
@@ -1,24 +1,95 @@
 <template>
     <div id="container">
-        <v-text-field
+        <form @submit.prevent="createMessage">
+            <v-text-field
               color="purple darken-2"
               label="Titre"
               id="title"
-        ></v-text-field>
+              v-model="title"
+            ></v-text-field>
 
-         <v-file-input id="file" truncate-length="15" v-model="image"></v-file-input>
+            <v-file-input id="file" truncate-length="15" v-model="attachment"></v-file-input>
 
-        <v-container id="textarea" fluid>
-            <v-textarea
-            clearable
-            clear-icon="mdi-close-circle"
-            label="Exprimez-vous ..."
-            ></v-textarea>
-        </v-container>
+            <v-container id="textarea" fluid>
+                <v-textarea
+                clearable
+                clear-icon="mdi-close-circle"
+                label="Exprimez-vous ..."
+                v-model="content"
+                ></v-textarea>
+            </v-container>
 
-        <input type="submit" id='submit' value="Envoyer">
+            <input type="submit" id='submit' value="Envoyer">
+        </form>
     </div>
 </template>
+
+
+<script>
+import VueJwtDecode from 'vue-jwt-decode';
+import axios from 'axios';
+export default {
+    name: 'CreateMessage',
+    data () {
+        return {
+            messages: null,
+            token: null,
+            title: null,
+            attachment: null,
+            content: null,
+            user: null 
+        }
+    },
+    methods: {
+        createMessage() {
+            this.token = localStorage.getItem("token");
+            this.user =VueJwtDecode.decode(this.token);
+            if (this.attachment == null) {
+                let formData = new FormData();
+                formData.append('title', this.title);
+                formData.append('content', this.content);
+                console.log(this.content);
+                console.log(this.title);
+                
+                axios.post('http://localhost:3000/api/messages', formData, {
+                    headers: {
+                        Authorization: "Bearer "  + this.token,
+                        'Content-Type': 'multipart/form-data'
+                    },
+                })
+                .then(response => {
+                    console.log(response);
+                    window.location.href='/main';
+                })
+                .catch(error => console.log(error));
+            } else {
+                let formData = new FormData();
+                formData.append('title', this.title);
+                formData.append('attachment', this.attachment);
+                formData.append('content', this.content);
+                
+
+                axios.post('http://localhost:3000/api/messages', formData, {
+                    headers: {
+                        Authorization: "Bearer "  + this.token,
+                        'Content-Type': 'multipart/form-data'
+                    },
+
+                })
+                .then(response => {
+                    console.log(response);
+                    window.location.href='/main';
+                })
+                .catch(error => console.log(error));
+            }
+            
+        }
+    }
+}
+</script>
+
+
+
 
 <style scoped>
 #container {
